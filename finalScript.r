02971a1533b95bb8135932cc69a0a5d2eb4584b0
radx64 <radek-team@wp.pl>
2014-05-17 20:07:31 +0200

    Few fixes in in final script

--- finalScript.r
+++ finalScript.r
@@ -1,8 +1,9 @@
 library(FSelector);
 library(cvTools);
+library(caret);
 library(nnet);
 
-filename <- "/home/radek/Pulpit/med/data.csv";
+filename <- "/home/radek/Pulpit/LTNN/data.csv";
 maxNumOfAtribs <- 20;
 numOfValidationRepetition <- 10;
 
@@ -14,8 +15,8 @@ cols <- c(1:22);
 
 FirstDataSet_1E <- FirstDataSet[cols];
 
-ranking <- rank.correlation (ClassificationGroup + PatientNo ~ ., data = FirstDataSet_1E);
-#ranking <- relief(ClassificationGroup + PatientNo ~ ., data = FirstDataSet_1E, neighbours.count = 5, sample.size = 10);
+#ranking <- rank.correlation (ClassificationGroup + PatientNo ~ ., data = FirstDataSet_1E);
+ranking <- relief(ClassificationGroup + PatientNo ~ ., data = FirstDataSet_1E, neighbours.count = 5, sample.size = 10);
 
 for(numOfAtribs in 20:maxNumOfAtribs)
 {
@@ -29,19 +30,22 @@ for(numOfAtribs in 20:maxNumOfAtribs)
   folds1 <- cvFolds(NROW(FirstDataSet_1E_SelectedRanks), K = numOfValidationRepetition);
   for(repetiton in 1:numOfValidationRepetition)
   {
-    train <- FirstDataSet_1E_SelectedRanks[folds1$subsets[folds1$which != repetiton], ]; # zbior testowy
-    validation <- FirstDataSet_1E_SelectedRanks[folds1$subsets[folds1$which == repetiton], ]; # zbor testujacy
+    train1 <- FirstDataSet_1E_SelectedRanks[folds1$subsets[folds1$which != repetiton], ]; # zbior testowy
+    validation1 <- FirstDataSet_1E_SelectedRanks[folds1$subsets[folds1$which == repetiton], ]; # zbor testujacy
     
-    #ann1 <- nnet(as.factor(ClassificationGroup) ~ ., data = train, size = 21, decay = 0); # atrybut size podobnie jak poprzednio
-    ann1 <- train(train, as.factor(train$ClassificationGroup), method = "nnet", 
-        tuneGrid=data.frame(.size = 13, .decay = 0 ),  # atrybut size wyliczasz za pomocą wzoru: (ilość cech wejściowych + ilość wyjściowych klas)/2
+    #ann1 <- nnet(as.factor(ClassificationGroup) ~ ., data = train1, size = 21, decay = 0); # atrybut size podobnie jak poprzednio
+
+    ann1 <- train(train1, as.factor(train1$ClassificationGroup), method = "nnet", 
+        tuneGrid=data.frame(.size = 20, .decay = 0 ),  # atrybut size wyliczasz za pomocą wzoru: (ilość cech wejściowych + ilość wyjściowych klas)/2
         # ewentualnie przy testowaniu sprawdzacz czy size+1, size+2, itd. ale bez przesady, są lepsze
-        trControl = trainControl(method = "cv", number = 2), 
+        trControl = trainControl(method = "repeatedcv", number = 2, repeats = 5), 
         tuneLength = 5);
-    #pred1 <- predict(ann1, newdata = validation, type = "class");
-    pred1 <- predict(ann1, newdata = validation, type = "raw");
+  
+    #pred1 <- predict(ann1, newdata = validation1, type = "class");
     
-    tabela <- table(validation$ClassificationGroup,pred1);
+    pred1 <- predict(ann1, newdata = validation1, type = "raw");
+    
+    tabela <- table(validation1$ClassificationGroup,pred1);
     
     for(row in 1:maxNumOfAtribs)
     {
@@ -54,26 +58,19 @@ for(numOfAtribs in 20:maxNumOfAtribs)
         }
         else
         {
-          output <- sprintf("Nie ma takiej kolumny: %d",row); print(output);
+          output <- sprintf("No column named %d found.",row); print(output);
         }
       }
       else
       {
-        output <- sprintf("Nie ma takiego wiersza: %d",row); print(output);
+        output <- sprintf("No row named %d found.",row); print(output);
       }
     }
     tries <- tries + sum(tabela);
     
   }
-  #print(pred1); 
   
-  
-  
-  
-
-  #print(tabela);
-  
-  output<-sprintf("Trafilismy %d razy z %d",hits, tries); print(output);
+  output<-sprintf("We've hit %d of %d times",hits, tries); print(output);
   output<-sprintf("%.3f %%",(hits/tries)*100.0); print(output);
 }
 
